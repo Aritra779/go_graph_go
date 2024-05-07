f59052da83ebea1f1927c4024632a65d7d727b48
Aritra779 <aritra.dutta.366@gmail.com>
2024-05-07 16:40:37 +0530

    feat: add and remove node

--- graph.go
+++ graph.go
@@ -1,5 +1,7 @@
 package go_graph_go
 
+import "errors"
+
 type Node struct {
 	id        string
 	Neighbors map[string]*Node
@@ -9,23 +11,47 @@ type Graph struct {
 	Nodes map[string]*Node
 }
 
+// Creates a new Graph
 func NewGraph() *Graph {
 	return &Graph{
 		Nodes: make(map[string]*Node),
 	}
 }
 
+// Adds a neighbor to the node
 func (node *Node) AddNeighbor(neighborNode *Node) {
 	node.Neighbors[neighborNode.id] = neighborNode
 }
 
+// removes a neighbor from a node
 func (node *Node) RemoveNeighbor(neighborNode *Node) {
-	node.Neighbors[neighborNode.id] = nil
+	delete(node.Neighbors, neighborNode.id)
 }
 
-func (graph *Graph) AddNode(node *Node) {}
+// Adds A Node with No Neighbors
+func (graph *Graph) AddNode(node *Node) error {
+	if _, exists := graph.Nodes[node.id]; !exists {
+		newNode := &Node{
+			id:        node.id,
+			Neighbors: make(map[string]*Node),
+		}
+		graph.Nodes[newNode.id] = newNode
+		return nil
+	}
+	return errors.New("node Already Exists in the graph")
+}
 
-func (graph *Graph) RemoveNode(node *Node) {}
+// Removes a node from the graph along with all the connected edges
+func (graph *Graph) RemoveNode(node *Node) error {
+	if _, exists := graph.Nodes[node.id]; !exists {
+		return errors.New("trying to delete a non existing Node")
+	}
+	for _, neighbor := range graph.Nodes[node.id].Neighbors {
+		neighbor.RemoveNeighbor(node)
+	}
+	delete(graph.Nodes, node.id)
+	return nil
+}
 
 func (graph *Graph) AddEdge(node1Id, node2Id string) {}
 
