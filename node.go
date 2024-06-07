0eeabe225cda58cd5897e334f8f389ebf1af5234
Aritra779 <aritra.dutta.366@gmail.com>
2024-06-07 11:25:56 +0530

    feat: mutex for all maps

--- node.go
+++ node.go
@@ -2,15 +2,21 @@ package graph
 
 // Adds a neighbor to the node
 func (node *Node) addNeighbor(neighborNode *Node) {
+	node.NeighborMutex.Lock()
+	defer node.NeighborMutex.Unlock()
 	node.Neighbors[neighborNode.Id] = neighborNode
 }
 
 // removes a neighbor from a node
 func (node *Node) removeNeighbor(neighborNode *Node) {
+	node.NeighborMutex.Lock()
+	defer node.NeighborMutex.Unlock()
 	delete(node.Neighbors, neighborNode.Id)
 }
 
 func (node *Node) isNeighbor(neighborNodeId string) bool {
+	node.NeighborMutex.RLock()
+	defer node.NeighborMutex.RUnlock()
 	_, ok := node.Neighbors[neighborNodeId]
 	return ok
 }
--- typeDefinition.go
+++ typeDefinition.go
@@ -1,11 +1,15 @@
 package graph
 
+import "sync"
+
 type Node struct {
-	Id        string
-	Data      any
-	Neighbors map[string]*Node
+	Id            string
+	Data          any
+	Neighbors     map[string]*Node
+	NeighborMutex sync.RWMutex
 }
 
 type Graph struct {
 	Nodes map[string]*Node
+	mutex sync.RWMutex
 }
